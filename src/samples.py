941f8643fa651691c24e4869f81bcf03c5197943
Datsudo <ekflfdv29@gmail.com>
2022-12-07 21:58:29 +0800

    Add fractal plant

--- src/samples.py
+++ src/samples.py
@@ -1,5 +1,8 @@
 from lsystem import LSystem
 
+import math
+
+
 def koch_curve(pen, num_gen: int) -> None:
     rules = {"F": "F+F-F-F+F"}
     axiom = "F"
@@ -41,3 +44,38 @@ def dragon_curve(pen, num_gen: int) -> None:
         if s in {"F", "G"}: pen.forward(10)
         elif s == "+": pen.left(90)
         else: pen.right(90)
+
+def fractal_plant(pen, num_gen: int) -> None:
+    rules = {
+        "F": "FF",
+        "X": "F+[[X]-X]-F[-FX]+X"
+    }
+    axiom = "X"
+
+    lsys = LSystem(axiom, rules, num_gen)
+    sentence = lsys.generate()
+
+
+    xpos, ypos = pen.pos()
+    theta = math.pi / 2 - 0.4
+    dtheta = math.radians(25)
+    positions = []
+    for s in sentence:
+        if s in {"F", "G"}:
+            curr_x = xpos + 5 * math.cos(theta)
+            curr_y = ypos + 5 * math.sin(theta)
+            pen.goto(curr_x, curr_y)
+            pen.down()
+            xpos, ypos = curr_x, curr_y
+        elif s == "+":
+            theta += dtheta
+            pen.setheading(theta)
+        elif s == "-":
+            theta -= dtheta
+            pen.setheading(theta)
+        elif s == "[":
+            positions.append({"x":xpos, "y":ypos, "theta": theta})
+        elif s == "]":
+            position = positions.pop()
+            xpos, ypos, theta = position['x'], position['y'], position['theta']
+            pen.up()
